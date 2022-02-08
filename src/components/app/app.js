f374e39bf134b84508b868f4d9d9c101781726e7
vtyric <misha.torno@gmail.com>
2022-02-08 17:36:29 +0500

    реализовал посик

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,8 @@ export default class App extends Component {
             {label: "открыть тестовое от чела", id: 2, important: false, done: true},
             {label: "поиграть в доту", id: 3, important: false, done: true},
             {label: "дойти до качалки", id: 4, important: true, done: false}
-        ]
+        ],
+        term: ''
     };
 
     deleteItem = (id) => this.setState(({todoData}) => ({todoData: todoData.filter(d => d.id !== id)}));
@@ -37,22 +38,27 @@ export default class App extends Component {
     onToggleImportant = (id) => this.setState(({todoData}) =>
         ({todoData: this.#toggleProperty(todoData, id, 'important')}));
 
+    onSearchLabelChange = (term) => this.setState({term});
+
+    search = (items, term) => items.filter(item => item.label.toLowerCase().includes(term.toLowerCase()))
+
     render() {
-        const {todoData} = this.state;
+        const {todoData, term} = this.state;
         const doneCount = todoData.filter(d => d.done).length;
         const todoCount = todoData.length - doneCount;
+        const items = this.search(todoData, term);
 
         return (
             <div className="app">
                 <AppHeader todo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel onSearchLabelChange={this.onSearchLabelChange}/>
                     <ItemStatusFilter/>
                 </div>
                 <TodoItemAddPanel
                     onItemAdd={this.addItem}/>
                 <TodoList
-                    data={todoData}
+                    data={items}
                     onDelete={this.deleteItem}
                     onToggleDone={this.onToggleDone}
                     onToggleImportant={this.onToggleImportant}/>
--- src/components/search-pannel/search-panel.js
+++ src/components/search-pannel/search-panel.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './search.panel.css';
 
-const SearchPanel = () => {
-    return (<input className="search form-control" placeholder="type to search" type="text"/>);
-};
+export default class SearchPanel extends Component {
+    onLabelChange = (e) => this.props.onSearchLabelChange(e.target.value)
 
-export default SearchPanel;
+    render() {
+        return (
+            <input
+                className="search form-control"
+                placeholder="type to search"
+                type="text"
+                onChange={this.onLabelChange}
+            />);
+    }
+}
+
+
